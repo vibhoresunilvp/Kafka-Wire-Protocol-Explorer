c5edcb1515c2128ce696e7f87cf0768fff4f71a5
Vibhore Singh <vibhore.sunil@virginpulse.com>
2025-12-06 20:10:57 +0530

    [any message]

--- app/main.py
+++ app/main.py
@@ -1,5 +1,10 @@
 import socket  # noqa: F401
 
+def parse_response() -> bytes:
+    message_size = "00000000"
+    correlation_id = "00000007"
+
+    return bytes.fromhex(message_size + correlation_id)
 
 def main():
     # You can use print statements as follows for debugging,
@@ -9,15 +14,8 @@ def main():
     # Uncomment this to pass the first stage
     
     server = socket.create_server(("localhost", 9092), reuse_port=True)
-    connection, address = server.accept() # wait for client
-    data = connection.recv(1024)
-    correlation_id = 7
-    print(data)
-    message_val = len(data).to_bytes(4, byteorder="big")
-    correlation_val = correlation_id.to_bytes(4, byteorder="big")
-    data = message_val+correlation_val
-    print(data.hex())
-    connection.sendall(data)
+    connection, _ = server.accept() # wait for client
+    connection.sendall(parse_response())
 
 
 if __name__ == "__main__":
