ee04ca9c580e3947dd89697eea585c12cb64a108
Vibhore Singh <vibhore.sunil@virginpulse.com>
2025-12-07 17:50:58 +0530

    [any message]

--- app/main.py
+++ app/main.py
@@ -1,21 +1,19 @@
 import socket  # noqa: F401
 
-def parse_response() -> bytes:
+def parse_response(data) -> bytes:
     message_size = "00000000"
-    correlation_id = "00000007"
+    correlation_id = data[8:12]
 
     return bytes.fromhex(message_size + correlation_id)
 
 def main():
-    # You can use print statements as follows for debugging,
-    # they'll be visible when running tests.
     print("Logs from your program will appear here!")
-
-    # Uncomment this to pass the first stage
     
     server = socket.create_server(("localhost", 9092), reuse_port=True)
     connection, _ = server.accept() # wait for client
-    connection.sendall(parse_response())
+    data = connection.recv(1024)
+    print(int.from_bytes(data[8:12], byteorder="big"))
+    connection.sendall(parse_response(data))
 
 
 if __name__ == "__main__":
