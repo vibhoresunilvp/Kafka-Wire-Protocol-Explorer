f2f197554c7a8e3d68ab5e7d2096d6e2db1f95f8
Vibhore Singh <vibhore.sunil@virginpulse.com>
2025-12-10 20:02:02 +0530

    Include error_code

--- app/main.py
+++ app/main.py
@@ -1,10 +1,20 @@
 import socket  # noqa: F401
 
 def parse_response(data) -> bytes:
-    message_size = "00000000"
     correlation_id = data[8:12]
+    api_version = data[6:8]
+    error_code = 35
+    api_version_hex = int.from_bytes(api_version, "big")
+    print(f"api_version: {api_version_hex}")
+    if(0 <= api_version_hex <= 4):
+        error_code = 0
+    error_code = error_code.to_bytes(2, "big")
+    print(f"error_code: {int.from_bytes(error_code, "big")}")
+    print(f"correlation_id: {int.from_bytes(correlation_id, "big")}")
+    print(f"error_code: {int.from_bytes(error_code, "big")}")
 
-    return bytes.fromhex(message_size + correlation_id)
+    message_size = (0).to_bytes(4, byteorder="big")
+    return message_size+correlation_id+error_code+message_size
 
 def main():
     print("Logs from your program will appear here!")
@@ -12,7 +22,6 @@ def main():
     server = socket.create_server(("localhost", 9092), reuse_port=True)
     connection, _ = server.accept() # wait for client
     data = connection.recv(1024)
-    print(int.from_bytes(data[8:12], byteorder="big"))
     connection.sendall(parse_response(data))
 
 
