d07e3b2eba6e6caf9fd9029dd3e8e5d48c971f6b
Vibhore Singh <vibhore.sunil@virginpulse.com>
2025-12-16 15:32:06 +0530

    Include concurrent connections

--- app/main.py
+++ app/main.py
@@ -1,4 +1,5 @@
 import socket  # noqa: F401
+from threading import Thread
 
 # Task 4: Handle ApiVersions requests
 
@@ -43,17 +44,41 @@ def parse_response(data) -> bytes:
 
     # 3. message_size = header + body
     message_size = (len(correlation_id) + len(body)).to_bytes(4, "big")
+    print(f"message_size: {int.from_bytes(message_size, 'big')}")
+    print(f"correlation_id: {correlation_id}")
+    print(f"error_code: {error_code}")
+    print(f"api_keys_array_len: {api_keys_array_len}")
+    print(f"api_key: {api_key}")
+    print(f"min_version: {min_version}")
+    print(f"max_version: {max_version}")
+    print(f"api_key_tag_buffer: {api_key_tag_buffer}")
+    print(f"throttle_time_ms: {throttle_time_ms}")
+    print(f"response_tag_buffer: {response_tag_buffer}")
 
     return message_size + correlation_id + body
+
+def handle_client(connection, addr):
+    try:
+        while True:
+            data = connection.recv(1024)
+            if(not data):
+                break
+            connection.sendall(parse_response(data))
+    except Exception as exception:
+        print("No Data")
+    finally:
+        connection.close()
 
 def main():
     print("Logs from your program will appear here!")
     
     server = socket.create_server(("localhost", 9092), reuse_port=True)
-    connection, _ = server.accept() # wait for client
-    data = connection.recv(1024)
-    connection.sendall(parse_response(data))
-
+    server.listen()
+    while True:
+        connection, addr = server.accept()
+    
+        thread = Thread(target=handle_client, args=(connection, addr), daemon=True)
+        thread.start()
 
 if __name__ == "__main__":
     main()
